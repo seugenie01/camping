287b736f5b86feb8d1dc87de2a8932af9b0315d1
Eugenie Santoni <santonieugenie@gmail.com>
2024-09-05 14:43:42 -0400

    Custom JavaScript Part 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log('javascript connected!');
     
-const carousel = new bootstrap.Carousel('#homeCarousel', {
+/*const carousel = new bootstrap.Carousel('#homeCarousel', {
     interval: 2000,
     pause: false
 })
@@ -17,4 +17,23 @@ const carouselPlay = document.getElementById('carouselPlay');
 carouselPlay.addEventListener('click', function() {
     console.log('cycle the carousel');
     carousel.cycle();
+}) */
+const carousel = new bootstrap.Carousel('#homeCarousel', {
+    interval: 5000,
+    pause: false
 })
+
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
+
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+})   
